adcf9361c306800916c617721873f0cc8b25f35b
Jacob Friis Saxberg <jacob@saxberg.dk>
2014-03-30 17:20:26 -0400

    push test.html

--- gitploy.sh
+++ gitploy.sh
@@ -5,6 +5,7 @@ git checkout gh-pages
 
 # get stuff from master
 git checkout master -- index.html
+git checkout master -- test.html
 git checkout master -- gignal
 git commit -m "Updates from master"
 
